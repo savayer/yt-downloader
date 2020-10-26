d31f176c208e11300b9eb0a6b2c55bd018ca101e
savayer <sava0094@gmail.com>
2020-10-26 20:16:15 +0200

    (fix) cyrilyc to latin to set into headers

--- server/index.js
+++ server/index.js
@@ -21,8 +21,8 @@ app.get('/audio', async (req, res, next) => {
 			return res.sendStatus(400);
 		}
 
-		const data = await ytdl.getBasicInfo(url)
-		const title = data.videoDetails.title || 'audio'
+		const data = await ytdl.getBasicInfo(url, {format: 'mp4'})
+		const title = rus_to_latin(data.videoDetails.title.replace(/\s/g, "_")) || 'audio'
 
 		res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
 		ytdl(url, {
@@ -43,7 +43,7 @@ app.get('/video', async (req, res, next) => {
 		}
 
 		const data = await ytdl.getBasicInfo(url)
-		const title = data.videoDetails.title || 'video'
+		const title = rus_to_latin(data.videoDetails.title.replace(/\s/g, "_")) || 'video'
 
 		res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
 		ytdl(url, { format: 'mp4' }).pipe(res);
@@ -51,3 +51,26 @@ app.get('/video', async (req, res, next) => {
 		console.error(err);
 	}
 });
+
+function rus_to_latin ( str ) {
+	const ru = {
+		'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+		'е': 'e', 'ё': 'e', 'ж': 'j', 'з': 'z', 'и': 'i',
+		'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o',
+		'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+		'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
+		'щ': 'shch', 'ы': 'y', 'э': 'e', 'ю': 'u', 'я': 'ya'
+	}, n_str = [];
+
+	str = str.replace(/[ъь]+/g, '').replace(/й/g, 'i');
+
+	for ( let i = 0; i < str.length; ++i ) {
+		n_str.push(
+			ru[ str[i] ]
+			|| ru[ str[i].toLowerCase() ] === undefined && str[i]
+			|| ru[ str[i].toLowerCase() ].toUpperCase()
+		);
+	}
+
+	return n_str.join('');
+}
